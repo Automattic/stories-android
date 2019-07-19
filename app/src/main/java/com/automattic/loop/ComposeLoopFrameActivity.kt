948255a96f91ba160ac41d113805eabd53a49b52
Mario Zorz <mariozorz@gmail.com>
2019-07-19 11:17:49 -0300

    Add navigation between MainActivity and ComposeLoopFrameActivity

--- app/src/main/java/com/automattic/loop/ComposeLoopFrameActivity.kt
+++ app/src/main/java/com/automattic/loop/ComposeLoopFrameActivity.kt
@@ -1,31 +1,22 @@
 package com.automattic.loop
 
-import android.net.Uri
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-import androidx.navigation.Navigation
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener {
-    override fun onFragmentInteraction(uri: Uri) {
-        // TODO: change OnFragmentInteractionListener for something relevant to our needs
-    }
+class ComposeLoopFrameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_composer)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-            Navigation.findNavController(this, R.id.nav_host_fragment)
-                .navigate(R.id.action_mainFragment_to_composeLoopFrameActivity)
-        }
+        // TODO: flesh out photoEditor under nav host here
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
