c626fc6133c7603225bfe093f53b68436f481f9d
Mario Zorz <mariozorz@gmail.com>
2019-07-29 13:34:06 -0300

    fixed Dialog imports and parent class

--- photoeditor/src/main/java/com/automattic/photoeditor/camera/ConfirmationDialog.kt
+++ photoeditor/src/main/java/com/automattic/photoeditor/camera/ConfirmationDialog.kt
@@ -20,15 +20,13 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.support.v4.app.DialogFragment
+import androidx.fragment.app.DialogFragment
 import com.automattic.photoeditor.R
-
 
 /**
  * Shows OK/Cancel confirmation dialog about camera permission.
  */
 class ConfirmationDialog : DialogFragment() {
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
             AlertDialog.Builder(activity)
                     .setMessage(R.string.request_permission)
--- photoeditor/src/main/java/com/automattic/photoeditor/camera/ErrorDialog.kt
+++ photoeditor/src/main/java/com/automattic/photoeditor/camera/ErrorDialog.kt
@@ -19,13 +19,12 @@ package com.automattic.photoeditor.camera
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.support.v4.app.DialogFragment
+import androidx.fragment.app.DialogFragment
 
 /**
  * Shows an error message dialog.
  */
 class ErrorDialog : DialogFragment() {
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
             AlertDialog.Builder(activity)
                     .setMessage(arguments?.getString(ARG_MESSAGE))
@@ -33,12 +32,10 @@ class ErrorDialog : DialogFragment() {
                     .create()
 
     companion object {
-
         @JvmStatic private val ARG_MESSAGE = "message"
 
         @JvmStatic fun newInstance(message: String): ErrorDialog = ErrorDialog().apply {
             arguments = Bundle().apply { putString(ARG_MESSAGE, message) }
         }
     }
-
 }
