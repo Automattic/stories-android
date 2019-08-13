cd781cfe2f231cd7ad2c4f169ffcf4162f642d7d
Mario Zorz <mariozorz@gmail.com>
2019-08-12 22:43:20 -0300

    changed sourceBitmap to nullable param type

--- photoeditor/src/main/java/com/automattic/photoeditor/views/PhotoEditorView.kt
+++ photoeditor/src/main/java/com/automattic/photoeditor/views/PhotoEditorView.kt
@@ -152,7 +152,7 @@ class PhotoEditorView : RelativeLayout {
         backgroundImage.setOnImageChangedListener(object : BackgroundImageView.OnImageChangedListener {
             override fun onBitmapLoaded(sourceBitmap: Bitmap?) {
                 imageFilterView.setFilterEffect(PhotoFilter.NONE)
-                imageFilterView.setSourceBitmap(sourceBitmap!!)
+                imageFilterView.setSourceBitmap(sourceBitmap)
                 Log.d(TAG, "onBitmapLoaded() called with: sourceBitmap = [$sourceBitmap]")
             }
         })
