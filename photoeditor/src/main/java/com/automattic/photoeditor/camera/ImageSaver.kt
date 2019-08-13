be97adf436ed8bfc53c3429531539681ea6d1b74
Mario Zorz <mariozorz@gmail.com>
2019-08-13 12:03:18 -0300

    removed unused import

--- photoeditor/src/main/java/com/automattic/photoeditor/camera/ImageSaver.kt
+++ photoeditor/src/main/java/com/automattic/photoeditor/camera/ImageSaver.kt
@@ -1,7 +1,6 @@
 package com.automattic.photoeditor.camera
 
 import android.media.Image
-import android.os.Handler
 import android.util.Log
 import com.automattic.photoeditor.camera.interfaces.ImageCaptureListener
 
