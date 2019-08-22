0832443f13b2c1e6fb8c9c799b49f3c80da89ffd
Mario Zorz <mariozorz@gmail.com>
2019-08-22 10:33:26 -0300

    moved constants to companion object

--- app/src/main/java/com/automattic/portkey/compose/ComposeLoopFrameActivity.kt
+++ app/src/main/java/com/automattic/portkey/compose/ComposeLoopFrameActivity.kt
@@ -51,9 +51,6 @@ class ComposeLoopFrameActivity : AppCompatActivity() {
     private lateinit var photoEditor: PhotoEditor
     private lateinit var backgroundSurfaceManager: BackgroundSurfaceManager
     private var progressDialog: ProgressDialog? = null
-    private val CAMERA_PREVIEW_LAUNCH_DELAY = 500L
-    private val CAMERA_VIDEO_RECORD_MAX_LENGTH_MS = 10000L
-    private val CAMERA_STILL_PICTURE_ANIM_MS = 300L
 
     private val timesUpRunnable = Runnable {
         stopRecordingVideo(false) // time's up, it's not a cancellation
@@ -583,5 +580,11 @@ class ComposeLoopFrameActivity : AppCompatActivity() {
             putInt(getString(string.pref_flash_mode_selection), flashModeSelection.id)
             commit()
         }
+    }
+
+    companion object {
+        private const val CAMERA_PREVIEW_LAUNCH_DELAY = 500L
+        private const val CAMERA_VIDEO_RECORD_MAX_LENGTH_MS = 10000L
+        private const val CAMERA_STILL_PICTURE_ANIM_MS = 300L
     }
 }
