a464820ecd9f7f70972289797ae40b802772927e
Mario Zorz <mariozorz@gmail.com>
2019-09-05 11:22:27 -0300

    fixed lint warning

--- app/src/main/java/com/automattic/portkey/util/MediaUtils.kt
+++ app/src/main/java/com/automattic/portkey/util/MediaUtils.kt
@@ -5,7 +5,8 @@ import java.util.Locale
 fun isVideo(url: String?): Boolean {
     var url: String? = url ?: return false
     url = url!!.toLowerCase(Locale.ROOT)
-    return (url.endsWith(".ogv") || url.endsWith(".mp4") || url.endsWith(".m4v") || url.endsWith(".mov")
-            || url.endsWith(".wmv") || url.endsWith(".avi") || url.endsWith(".mpg") || url.endsWith(".3gp")
-            || url.endsWith(".3g2") || url.contains("video"))
+    return (url.endsWith(".ogv") || url.endsWith(".mp4") || url.endsWith(".m4v") ||
+            url.endsWith(".mov") || url.endsWith(".wmv") || url.endsWith(".avi") ||
+            url.endsWith(".mpg") || url.endsWith(".3gp") || url.endsWith(".3g2") ||
+            url.contains("video"))
 }
