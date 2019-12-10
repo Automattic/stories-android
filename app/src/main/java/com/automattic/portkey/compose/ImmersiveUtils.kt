a14ddd1b0dc688a38702077982a65f2d0ed397aa
Mario Zorz <mariozorz@gmail.com>
2019-12-10 19:39:20 -0300

    moved flags from styles to utils file for consistency

--- app/src/main/java/com/automattic/portkey/compose/ImmersiveUtils.kt
+++ app/src/main/java/com/automattic/portkey/compose/ImmersiveUtils.kt
@@ -20,7 +20,9 @@ fun hideSystemUI(window: Window) {
 }
 
 fun hideStatusBar(window: Window) {
-    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+    window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
 }
 
 // Shows the system bars by removing all the flags
