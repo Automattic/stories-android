030207e63e4fa731a2f9c4b365ebb68269d11cdf
Mario Zorz <mariozorz@gmail.com>
2019-12-13 11:23:22 -0300

    added new BottomStripFragment

---
+++ app/src/main/java/com/automattic/portkey/compose/story/BottomStripFragment.kt
@@ -0,0 +1,18 @@
+package com.automattic.portkey.compose.story
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.automattic.portkey.R.layout
+
+open class BottomStripFragment: Fragment () {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layout.fragment_bottom_strip, container, false)
+    }
+}
