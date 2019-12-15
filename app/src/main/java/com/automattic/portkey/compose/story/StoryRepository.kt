ab982a9f3548dc99bc78edfeba3e7b5430bbf429
Mario Zorz <mariozorz@gmail.com>
2019-12-15 19:53:21 -0300

    using frame array in the StoryRepository so it's easier to have access

--- app/src/main/java/com/automattic/portkey/compose/story/StoryRepository.kt
+++ app/src/main/java/com/automattic/portkey/compose/story/StoryRepository.kt
@@ -1,33 +1,33 @@
 package com.automattic.portkey.compose.story
 
 class StoryRepository {
-    private var currentStory = Story(ArrayList())
+    private var currentStoryFrames = ArrayList<StoryFrameItem>()
     private var currentSelectedFrameIndex = 0
     private val stories = ArrayList<Story>()
     fun loadStory(storyIndex: Int): ArrayList<StoryFrameItem> {
         if (stories.size > storyIndex) {
-            currentStory = stories[storyIndex]
+            currentStoryFrames = stories[storyIndex].frames
         } else {
             // just crete a new story if we didn't find such index
-            currentStory = Story(ArrayList())
+            currentStoryFrames = ArrayList()
         }
-        return currentStory.frames
+        return currentStoryFrames
     }
 
     fun addStoryFrameItemToCurrentStory(item: StoryFrameItem) {
-        currentStory.frames.add(0, item)
+        currentStoryFrames.add(0, item)
     }
 
     // when the user finishes a story, just add it to our repo for now and clear currentStory
     fun finishCurrentStory() {
-        stories.add(currentStory)
-        currentStory = Story(ArrayList())
+        stories.add(Story(currentStoryFrames)) // create new Story wrapper with the finished frames in there
+        currentStoryFrames = ArrayList()
     }
 
     // used when user taps on a different frame and keep track record of its state
     fun setSelectedFrame(index: Int): StoryFrameItem {
         currentSelectedFrameIndex = index
-        return currentStory.frames[index]
+        return currentStoryFrames[index]
     }
 
     companion object {
