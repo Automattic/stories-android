f1d00059d7899006513f4da9a1b95ef2b7e06fdf
Mario Zorz <mariozorz@gmail.com>
2019-12-16 18:02:20 -0300

    added kotlinx-serialization plugin and also bumped Kotlin version to 1.3.61 given the latest serialization needs it form 0.14.0 and on https://github.com/Kotlin/kotlinx.serialization#incompatible-changes

--- app/src/main/java/com/automattic/portkey/compose/story/Story.kt
+++ app/src/main/java/com/automattic/portkey/compose/story/Story.kt
@@ -1,3 +1,6 @@
 package com.automattic.portkey.compose.story
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Story(val frames: ArrayList<StoryFrameItem>)
--- app/src/main/java/com/automattic/portkey/compose/story/StoryFrameItem.kt
+++ app/src/main/java/com/automattic/portkey/compose/story/StoryFrameItem.kt
@@ -1,5 +1,7 @@
 package com.automattic.portkey.compose.story
 
 import com.automattic.portkey.compose.story.StoryFrameItemType.IMAGE
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class StoryFrameItem(val filePath: String, val frameItemType: StoryFrameItemType = IMAGE, val name: String? = null)
