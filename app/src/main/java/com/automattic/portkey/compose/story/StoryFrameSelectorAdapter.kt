b93b7a4024d1e17c296d98f963e1f8d37d7a4089
Mario Zorz <mariozorz@gmail.com>
2019-12-15 21:03:32 -0300

    now showing the actual captured image in the bottom strip

--- app/src/main/java/com/automattic/portkey/compose/story/StoryFrameSelectorAdapter.kt
+++ app/src/main/java/com/automattic/portkey/compose/story/StoryFrameSelectorAdapter.kt
@@ -48,7 +48,7 @@ class StoryFrameSelectorAdapter(
             }
 
             Glide.with(context)
-                .load(R.drawable.intro02) // TODO change for data coming from datasource at [position]
+                .load(storyFrameItemsWithPlusControl.frames[position].filePath)
                 .transform(CenterCrop(), RoundedCorners(16))
                 .into(holder.imageView)
 
