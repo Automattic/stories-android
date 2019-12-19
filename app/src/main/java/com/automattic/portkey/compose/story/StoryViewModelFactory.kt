753f1e30753606ada22f6c44d074024240ca16af
Mario Zorz <mariozorz@gmail.com>
2019-12-19 08:12:44 -0300

    fixed unchecked cast warning

--- app/src/main/java/com/automattic/portkey/compose/story/StoryViewModelFactory.kt
+++ app/src/main/java/com/automattic/portkey/compose/story/StoryViewModelFactory.kt
@@ -6,6 +6,10 @@ import androidx.lifecycle.ViewModelProvider
 class StoryViewModelFactory(private val repository: StoryRepository, private val storyId: Int) :
     ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return StoryViewModel(repository, storyId) as T
+            if (modelClass.isAssignableFrom(StoryViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return StoryViewModel(repository, storyId) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
         }
 }
