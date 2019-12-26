a984bacde30f5086da3aef55c1bc53144d83278a
Mario Zorz <mariozorz@gmail.com>
2019-12-26 10:50:59 -0300

    separating uiState from Repository data
    - using sealed class for StoryFrameHolder, which can be either of two types to hold the plus icon button and the rest of actual Story items on the RecyclerView adapter.
    - introduced the concept of `uiState` in the ViewModel, consisting changes to the UI state then pushing to LiveData so it gets reflected on the View. The `uiState` is a LiveData on `StoryFrameListUiState`, an array of `StoryFrameListItemUiState`.
    - The `uiState` then holds only data for the UI, while the Model lives in the Repository. These were hard to grasp at first for me given they hold pretty much the same information (see <> and <>), but it makes a lot of sense to bring separation of concerns among them.
    - the clickable events have been separated. Views have `OnClickListener` but these call a SingleLiveEvent once clicked, to separate both.

--- app/src/main/java/com/automattic/portkey/compose/story/StoryFrameSelectorFragment.kt
+++ app/src/main/java/com/automattic/portkey/compose/story/StoryFrameSelectorFragment.kt
@@ -8,10 +8,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.automattic.portkey.R.layout
+import com.automattic.portkey.compose.story.StoryViewModel.StoryFrameListUiState
+import kotlinx.android.synthetic.main.fragment_story_frame_selector.*
 import kotlinx.android.synthetic.main.fragment_story_frame_selector.view.*
 
 open class StoryFrameSelectorFragment : Fragment() {
-    lateinit var adapter: StoryFrameSelectorAdapter
     lateinit var storyViewModel: StoryViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,18 +21,14 @@ open class StoryFrameSelectorFragment : Fragment() {
                 ViewModelProviders.of(requireActivity(), // important to use Activity's context, so we don't
                         // end up looking into the wrong ViewModelProviders bucket key
                         StoryViewModelFactory(StoryRepository, 0))[StoryViewModel::class.java]
-        storyViewModel.onStoryFrameItems.observe(this, Observer<List<StoryFrameItem>> { frames ->
-            // update adapter
-            adapter.addAllItems(frames)
-        })
 
         storyViewModel.onSelectedFrameIndex.observe(this, Observer<Pair<Int, Int>> { selectedFrameIndexChange ->
-            if (selectedFrameIndexChange.first == selectedFrameIndexChange.second) {
-                // just call it once
-                adapter.notifyItemChanged(selectedFrameIndexChange.first + 1)
-            } else {
-                adapter.notifyItemChanged(selectedFrameIndexChange.first + 1)
-                adapter.notifyItemChanged(selectedFrameIndexChange.second + 1)
+            updateContentUiStateSelection(selectedFrameIndexChange.first, selectedFrameIndexChange.second)
+        })
+
+        storyViewModel.uiState.observe(this, Observer { uiState ->
+            uiState?.let {
+                updateContentUiState(uiState)
             }
         })
 
@@ -44,10 +41,24 @@ open class StoryFrameSelectorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(layout.fragment_story_frame_selector, container, false)
-        // instantiate adapter with an empty Story until it gets loaded
-        adapter = StoryFrameSelectorAdapter(requireContext(), storyViewModel)
-        view.story_frames_view.adapter = adapter
+        view.story_frames_view.adapter = StoryFrameSelectorAdapter()
+        // TODO storyIndex here is hardcoded to 0, will need to change once we have multiple stories stored.
+        storyViewModel.loadStory(0)
         return view
+    }
+
+    private fun updateContentUiState(contentState: StoryFrameListUiState) {
+        (story_frames_view.adapter as StoryFrameSelectorAdapter).addAllItems(contentState.items)
+    }
+
+    private fun updateContentUiStateSelection(oldSelection: Int, newSelection: Int) {
+        if (oldSelection == newSelection) {
+            // just call it once
+            (story_frames_view.adapter as StoryFrameSelectorAdapter).notifyItemChanged(oldSelection)
+        } else {
+            (story_frames_view.adapter as StoryFrameSelectorAdapter).notifyItemChanged(oldSelection)
+            (story_frames_view.adapter as StoryFrameSelectorAdapter).notifyItemChanged(newSelection)
+        }
     }
 
     fun show() {
--- app/src/main/java/com/automattic/portkey/compose/story/StoryViewModel.kt
+++ app/src/main/java/com/automattic/portkey/compose/story/StoryViewModel.kt
@@ -3,16 +3,14 @@ package com.automattic.portkey.compose.story
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.automattic.portkey.compose.story.StoryViewModel.StoryFrameListItemUiState.StoryFrameListItemUiStateFrame
+import com.automattic.portkey.compose.story.StoryViewModel.StoryFrameListItemUiState.StoryFrameListItemUiStatePlusIcon
 
 class StoryViewModel(val repository: StoryRepository, val storyIndex: Int) : ViewModel() {
     private var currentSelectedFrameIndex: Int = DEFAULT_SELECTION
 
-    private val _onStoryFrameItems: MutableLiveData<List<StoryFrameItem>> by lazy {
-        MutableLiveData<List<StoryFrameItem>>().also {
-            it.value = repository.loadStory(storyIndex)
-        }
-    }
-    val onStoryFrameItems: LiveData<List<StoryFrameItem>> = _onStoryFrameItems
+    private val _uiState: MutableLiveData<StoryFrameListUiState> = MutableLiveData()
+    val uiState: LiveData<StoryFrameListUiState> = _uiState
 
     private val _onSelectedFrameIndex: MutableLiveData<Pair<Int, Int>> by lazy {
         MutableLiveData<Pair<Int, Int>>().also {
@@ -22,20 +20,21 @@ class StoryViewModel(val repository: StoryRepository, val storyIndex: Int) : Vie
     val onSelectedFrameIndex: LiveData<Pair<Int, Int>> = _onSelectedFrameIndex
 
     fun loadStory(storyIndex: Int) {
-        val framesInStory = repository.loadStory(storyIndex)
-        _onStoryFrameItems.value = framesInStory
-        _onSelectedFrameIndex.value = Pair(0, 0) // default selected frame when loading a new Story
+        repository.loadStory(storyIndex)
+        updateUiState(createUiStateFromModelState(repository.getImmutableCurrentStoryFrames()))
+        // default selected frame when loading a new Story
+        _onSelectedFrameIndex.value = Pair(DEFAULT_SELECTION, DEFAULT_SELECTION)
     }
 
     fun addStoryFrameItemToCurrentStory(item: StoryFrameItem) {
         repository.addStoryFrameItemToCurrentStory(item)
-        _onStoryFrameItems.value = repository.getImmutableCurrentStoryFrames()
+        updateUiState(createUiStateFromModelState(repository.getImmutableCurrentStoryFrames()))
     }
 
     // when the user finishes a story, just add it to our repo for now and clear currentStory
     fun finishCurrentStory() {
         repository.finishCurrentStory()
-        _onStoryFrameItems.value = repository.getImmutableCurrentStoryFrames()
+        updateUiState(createUiStateFromModelState(repository.getImmutableCurrentStoryFrames()))
         currentSelectedFrameIndex = DEFAULT_SELECTION // default selected frame when loading a new Story
         _onSelectedFrameIndex.value = Pair(DEFAULT_SELECTION, currentSelectedFrameIndex)
     }
@@ -44,7 +43,7 @@ class StoryViewModel(val repository: StoryRepository, val storyIndex: Int) : Vie
         repository.discardCurrentStory()
         currentSelectedFrameIndex = DEFAULT_SELECTION // default selected frame when loading a new Story
         _onSelectedFrameIndex.value = Pair(DEFAULT_SELECTION, currentSelectedFrameIndex)
-        _onStoryFrameItems.value = repository.getImmutableCurrentStoryFrames()
+        updateUiState(createUiStateFromModelState(repository.getImmutableCurrentStoryFrames()))
     }
 
     // used when user taps on a different frame and keep track record of its state
@@ -52,12 +51,63 @@ class StoryViewModel(val repository: StoryRepository, val storyIndex: Int) : Vie
         val newlySelectedFrame = repository.getImmutableCurrentStoryFrames()[index]
         val oldIndex = currentSelectedFrameIndex
         currentSelectedFrameIndex = index
-        _onSelectedFrameIndex.value = Pair(oldIndex, index)
+        updateUiStateForSelection(oldIndex, index)
         return newlySelectedFrame
     }
 
     fun getSelectedFrameIndex(): Int {
         return currentSelectedFrameIndex
+    }
+
+    private fun updateUiState(uiState: StoryFrameListUiState) {
+        _uiState.value = uiState
+    }
+
+    private fun updateUiStateForSelection(oldSelectedIndex: Int, newSelectedIndex: Int) {
+        val immutableStory: StoryFrameListUiState? = _uiState.value
+        immutableStory?.let {
+            if (it.items[oldSelectedIndex + 1] is StoryFrameListItemUiStateFrame) {
+                (it.items[oldSelectedIndex + 1] as StoryFrameListItemUiStateFrame).selected = false
+            }
+            if (it.items[newSelectedIndex + 1] is StoryFrameListItemUiStateFrame) {
+                (it.items[newSelectedIndex + 1] as StoryFrameListItemUiStateFrame).selected = true
+            }
+            _onSelectedFrameIndex.value = Pair(oldSelectedIndex + 1, newSelectedIndex + 1)
+        }
+    }
+
+    private fun createUiStateFromModelState(storyItems: List<StoryFrameItem>): StoryFrameListUiState {
+        val uiStateItems = ArrayList<StoryFrameListItemUiState>()
+        val newUiState = StoryFrameListUiState(uiStateItems)
+        // add the plus icon to the UiState array
+        uiStateItems.add(StoryFrameListItemUiStatePlusIcon)
+        // TODO add the SingleLiveEvent click listener
+        StoryFrameListItemUiStatePlusIcon.onItemTapped = {
+            // setSelectedFrame(index)
+            // TODO here trigger SingleLiveEvent
+        }
+        storyItems.forEachIndexed { index, model ->
+            val isSelected = (getSelectedFrameIndex() == index)
+            val oneFrameUiState = StoryFrameListItemUiStateFrame(selected = isSelected, filePath = model.filePath)
+            oneFrameUiState.onItemTapped = {
+                setSelectedFrame(index)
+            }
+            uiStateItems.add(oneFrameUiState)
+        }
+        return newUiState
+    }
+
+    data class StoryFrameListUiState(val items: List<StoryFrameListItemUiState>)
+
+    sealed class StoryFrameListItemUiState() {
+        var onItemTapped: (() -> Unit)? = null
+
+        object StoryFrameListItemUiStatePlusIcon : StoryFrameListItemUiState()
+
+        data class StoryFrameListItemUiStateFrame(
+            var selected: Boolean = false,
+            var filePath: String? = null
+        ) : StoryFrameListItemUiState()
     }
 
     companion object {
