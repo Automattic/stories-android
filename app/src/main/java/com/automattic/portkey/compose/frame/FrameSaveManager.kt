dca3d523c60b3d57c9f9ca4ad9283dad4baa2216
Mario Zorz <mariozorz@gmail.com>
2020-01-15 15:50:09 -0300

    removing PhotoEditorView layout transition animation then readding it back after clearing and adding Frame's AddedViews to avoid capturing an uncomplete background

--- app/src/main/java/com/automattic/portkey/compose/frame/FrameSaveManager.kt
+++ app/src/main/java/com/automattic/portkey/compose/frame/FrameSaveManager.kt
@@ -1,7 +1,6 @@
 package com.automattic.portkey.compose.frame
 
 import android.content.Context
-import android.util.Log
 import com.automattic.photoeditor.PhotoEditor
 import com.automattic.photoeditor.SaveSettings
 import com.automattic.photoeditor.util.FileUtils
@@ -21,53 +20,50 @@ class FrameSaveManager : CoroutineScope { // } by MainScope() {
         get() = Dispatchers.Main + job
 
     suspend fun saveImageFrame(context: Context, frame: StoryFrameItem, photoEditor: PhotoEditor): File {
-        // TODO fix the "video: false" parameter here and make a distinction on frame types here (VIDEO, IMAGE, etc)
-
         // creating a new file shouldn't be an expensive operation so, not switching Coroutine context here but staying
         // on the Main dispatcher seems reasonable
+        // TODO fix the "video: false" parameter here and make a distinction on frame types here (VIDEO, IMAGE, etc)
         val file = FileUtils.getLoopFrameFile(context, false)
         file.createNewFile()
 
-        Log.d("PORTKEY", "FrameSaveManager.saveImageFrame 2 ")
         val saveSettings = SaveSettings.Builder()
             .setClearViewsEnabled(true)
             .setTransparencyEnabled(false)
             .build()
 
-        Log.d("PORTKEY", "FrameSaveManager.saveImageFrame 3")
-        prepareCanvasForSnapshot(context, frame, photoEditor)
+        preparePhotoEditorViewForSnapshot(context, frame, photoEditor)
 
-        // switching coroutine to Dispatchers.IO to write image to file
+        // switching coroutine to Dispatchers.IO scope to write image to file
         withContext(Dispatchers.IO) {
-            Log.d("PORTKEY", "FrameSaveManager.saveImageFrame 4")
             FileUtils.saveViewToFile(file.absolutePath, saveSettings, photoEditor.composedCanvas)
         }
 
-        Log.d("PORTKEY", "FrameSaveManager.saveImageFrame 5 ")
         return file
     }
 
-    private fun prepareCanvasForSnapshot(context: Context, frame: StoryFrameItem, photoEditor: PhotoEditor) {
-        Log.d("PORTKEY", "FrameSaveManager.prepareCanvasForSnapshot 1")
-        Log.d("PORTKEY", "FrameSaveManager.prepareCanvasForSnapshot 2")
-        // now clear addedViews so we don't leak View.Context
-//            photoEditor.clearAllViews()
-//            photoEditor.composedCanvas.removeAllViews()
+    private fun preparePhotoEditorViewForSnapshot(context: Context, frame: StoryFrameItem, photoEditor: PhotoEditor) {
+        // disable layout change animations, we need this to make added views immediately visible, otherwise
+        // we may end up capturing a Bitmap of a backing drawable that still has not been updated
+        // (i.e. no visible added Views)
+        val transition = photoEditor.composedCanvas.getLayoutTransition()
+        photoEditor.composedCanvas.layoutTransition = null
 
-        Log.d("PORTKEY", "FrameSaveManager.prepareCanvasForSnapshot 3")
+        // now clear addedViews so we don't leak View.Context
+        photoEditor.clearAllViews()
+
         Glide.with(context)
             .load(frame.source.file ?: frame.source.contentUri)
             .transform(CenterCrop())
             .into(photoEditor.source)
 
-        Log.d("PORTKEY", "FrameSaveManager.prepareCanvasForSnapshot 4")
         // now call addViewToParent the addedViews remembered by this frame
-//            frame.addedViews.let {
-//                for (oneView in it) {
-//                    photoEditor.addViewToParent(oneView.view, oneView.viewType)
-//                }
-//            }
-//            photoEditor.composedCanvas.requestLayout()
-        Log.d("PORTKEY", "FrameSaveManager.prepareCanvasForSnapshot 5")
+        frame.addedViews.let {
+            for (oneView in it) {
+                photoEditor.addViewToParent(oneView.view, oneView.viewType)
+            }
+        }
+
+        // re-enable layout change animations
+        photoEditor.composedCanvas.layoutTransition = transition
     }
 }
