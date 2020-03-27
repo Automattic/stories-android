0e3ec323754ff9e8c0ee570e63b029b4499b159f
Mario Zorz <mariozorz@gmail.com>
2020-03-27 19:15:28 -0300

    added EventBus listeners for start / SaveResult signals, and normal snackbars

--- app/src/main/java/com/automattic/portkey/MainActivity.kt
+++ app/src/main/java/com/automattic/portkey/MainActivity.kt
@@ -6,8 +6,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import com.automattic.photoeditor.util.PermissionUtils
+import com.automattic.portkey.compose.frame.FrameSaveService.StorySaveProcessStart
+import com.automattic.portkey.compose.frame.FrameSaveService.StorySaveResult
+import com.automattic.portkey.compose.story.StoryRepository
 import com.automattic.portkey.intro.IntroActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener {
     override fun onFragmentInteraction(uri: Uri) {
@@ -18,6 +25,7 @@ class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionList
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        EventBus.getDefault().register(this)
 
         activity_main.setOnApplyWindowInsetsListener { view, insets ->
             // remember the insetTop as margin to all controls appearing at the top of the screen for full screen
@@ -45,4 +53,31 @@ class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionList
 
     override fun onSupportNavigateUp() =
         Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
+
+    override fun onDestroy() {
+        EventBus.getDefault().unregister(this)
+        super.onDestroy()
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onStorySaveResult(event: StorySaveResult) {
+        EventBus.getDefault().removeStickyEvent(event)
+        // TODO show snackbar and add the PendingIntent with the StorySaveResult as a Serialized object if errors
+        // TODO replace this with calls to snackbarSequencer.enqueue() when integrating code in WPAndroid
+        val text = String.format(
+            getString(R.string.story_saving_snackbar_finished_successfully),
+            StoryRepository.getStoryAtIndex(event.storyIndex).title
+        )
+        Snackbar.make(findViewById(android.R.id.content), text, Snackbar.LENGTH_SHORT).show()
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onStorySaveStart(event: StorySaveProcessStart) {
+        // TODO replace this with calls to snackbarSequencer.enqueue() when integrating code in WPAndroid
+        val text = String.format(
+            getString(R.string.story_saving_snackbar_started),
+            StoryRepository.getStoryAtIndex(event.storyIndex).title
+        )
+        Snackbar.make(findViewById(android.R.id.content), text, Snackbar.LENGTH_SHORT).show()
+    }
 }
