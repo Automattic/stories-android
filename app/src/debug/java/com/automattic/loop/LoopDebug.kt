183e3c2fa5e72d5d8a65d2ed929510478c768e08
Alex Forcier <alex.forcier@automattic.com>
2020-07-29 15:10:11 +0900

    Add Flipper support for demo app debug builds

--- app/src/debug/java/com/automattic/loop/LoopDebug.kt
+++ app/src/debug/java/com/automattic/loop/LoopDebug.kt
@@ -1,7 +1,20 @@
 package com.automattic.loop
 
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
+
 class LoopDebug : Loop() {
     override fun onCreate() {
         super.onCreate()
+
+        if (FlipperUtils.shouldEnableFlipper(this)) {
+            SoLoader.init(this, false)
+            AndroidFlipperClient.getInstance(this).apply {
+                addPlugin(InspectorFlipperPlugin(applicationContext, DescriptorMapping.withDefaults()))
+            }.start()
+        }
     }
 }
