b269da88bb92751a30e7271413ebe37eb2f0bb40
Mario Zorz <mariozorz@gmail.com>
2020-07-29 19:48:23 -0300

    adjusting width correctly now if the calculated height for target ratio on fixed width ends up being larger than original height

--- stories/src/main/java/com/wordpress/stories/util/ViewUtils.kt
+++ stories/src/main/java/com/wordpress/stories/util/ViewUtils.kt
@@ -19,7 +19,7 @@ fun cloneViewSpecs(originalView: View, targetView: View, doNormalizeTo9_16: Bool
     var heightToUse = originalView.height
 
     if (doNormalizeTo9_16) {
-        val adjustedSize = normalizeHeightExportTo916(originalView.width, originalView.height)
+        val adjustedSize = normalizeSizeExportTo916(originalView.width, originalView.height)
         heightToUse = adjustedSize.height
         widthToUse = adjustedSize.width
     }
@@ -31,7 +31,7 @@ fun cloneViewSpecs(originalView: View, targetView: View, doNormalizeTo9_16: Bool
     targetView.layout(0, 0, targetView.measuredWidth, targetView.measuredHeight)
 }
 
-fun normalizeHeightExportTo916(originalWidth: Int, originalHeight: Int): Size {
+fun normalizeSizeExportTo916(originalWidth: Int, originalHeight: Int): Size {
     /*
         1. if the screen is 16:9, we're OK
         2. if not, resolve what the height should be for the screen's given width.
@@ -49,14 +49,13 @@ fun normalizeHeightExportTo916(originalWidth: Int, originalHeight: Int): Size {
         val normalizedHeightShouldBe = originalWidth / targetRatio
         if (normalizedHeightShouldBe <= originalHeight) {
             // 3. if the result of (2) is less than actual height, set the new target height for the cloned
-            // view to the number calculated as of (2) (cropping)
+            // view to the number calculated as of (2) (cropping on height)
             return Size(originalWidth, normalizedHeightShouldBe.toInt())
         } else {
-            // (less likely to happen in practice)
             // 4. if the result of (2) is greater than actual height --> crop the width to match the ratio needed
-            // for the given height
-            val normalizedWidthShouldBe = normalizedHeightShouldBe * targetRatio
-            return Size(normalizedWidthShouldBe.toInt(), normalizedHeightShouldBe.toInt())
+            // for the given original height (cropping on width)
+            val normalizedWidthShouldBe = originalHeight * targetRatio
+            return Size(normalizedWidthShouldBe.toInt(), originalHeight)
         }
     }
 }
