d15ad2018f84010cf7f0992851316c0f020997aa
Mario Zorz <mariozorz@gmail.com>
2020-07-30 09:50:37 -0300

    elevated TARGET_RATIO_9_16 to a constant, simplified check

--- stories/src/main/java/com/wordpress/stories/util/ViewUtils.kt
+++ stories/src/main/java/com/wordpress/stories/util/ViewUtils.kt
@@ -5,6 +5,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.children
 
+const val TARGET_RATIO_9_16 = 0.5625f // 9:16
+
 fun removeViewFromParent(view: View) {
     view.parent?.let {
         it as ViewGroup
@@ -31,22 +33,24 @@ fun cloneViewSpecs(originalView: View, targetView: View, doNormalizeTo9_16: Bool
     targetView.layout(0, 0, targetView.measuredWidth, targetView.measuredHeight)
 }
 
+fun isSizeRatio916(originalWidth: Int, originalHeight: Int): Boolean {
+    return (originalWidth.toFloat() / originalHeight.toFloat()) == TARGET_RATIO_9_16
+}
+
 fun normalizeSizeExportTo916(originalWidth: Int, originalHeight: Int): Size {
     /*
         1. if the screen is 16:9, we're OK
         2. if not, resolve what the height should be for the screen's given width.
         3. if the result of (2) is less than actual height, set the new target
             height for the cloned view to the number calculated as of (2) (cropping)
-        4. (very unlikely) if the result of (2) is greater than actual height --> crop on width to make it fit 9:16
+        4. if the result of (2) is greater than actual height --> crop on width to make it fit 9:16
      */
-    val targetRatio = 0.5625f // 9:16
-
-    if ((originalWidth.toFloat() / originalHeight.toFloat()) == targetRatio) {
-        // 1. if the screen is 16:9, we're OK
+    if (isSizeRatio916(originalWidth, originalHeight)) {
+        // 1. if the screen is already 16:9, we're OK, return the original width and height
         return Size(originalWidth, originalHeight)
     } else {
         // 2. if not, resolve what the height should be for the screen's given width.
-        val normalizedHeightShouldBe = originalWidth / targetRatio
+        val normalizedHeightShouldBe = originalWidth / TARGET_RATIO_9_16
         if (normalizedHeightShouldBe <= originalHeight) {
             // 3. if the result of (2) is less than actual height, set the new target height for the cloned
             // view to the number calculated as of (2) (cropping on height)
@@ -54,7 +58,7 @@ fun normalizeSizeExportTo916(originalWidth: Int, originalHeight: Int): Size {
         } else {
             // 4. if the result of (2) is greater than actual height --> crop the width to match the ratio needed
             // for the given original height (cropping on width)
-            val normalizedWidthShouldBe = originalHeight * targetRatio
+            val normalizedWidthShouldBe = originalHeight * TARGET_RATIO_9_16
             return Size(normalizedWidthShouldBe.toInt(), originalHeight)
         }
     }
