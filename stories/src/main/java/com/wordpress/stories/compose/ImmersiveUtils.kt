e7bf4c8dd0c1013b2d15d5a69bbce81352c19a59
Mario Zorz <mariozorz@gmail.com>
2020-08-07 12:33:46 -0300

    make navigation bar translucent but always present

--- stories/src/main/java/com/wordpress/stories/compose/ImmersiveUtils.kt
+++ stories/src/main/java/com/wordpress/stories/compose/ImmersiveUtils.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
 import android.view.Window
+import android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
 
 const val IMMERSIVE_FLAG_TIMEOUT = 500L
 /** Combination of all flags required to put activity into immersive mode */
@@ -23,6 +24,10 @@ fun hideStatusBar(window: Window) {
     window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
             or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    window.setFlags(
+        FLAG_TRANSLUCENT_NAVIGATION,
+        FLAG_TRANSLUCENT_NAVIGATION
+    )
 }
 
 // Shows the system bars by removing all the flags
