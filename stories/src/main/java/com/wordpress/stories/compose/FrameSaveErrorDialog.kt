f6f7b566178aa6406cb700978540e361d5f294f6
Mario Zorz <mariozorz@gmail.com>
2020-08-24 20:37:37 -0300

    removed unused import

--- stories/src/main/java/com/wordpress/stories/compose/FrameSaveErrorDialog.kt
+++ stories/src/main/java/com/wordpress/stories/compose/FrameSaveErrorDialog.kt
@@ -4,7 +4,6 @@ import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.wordpress.stories.R.style
 
 interface FrameSaveErrorDialogOk {
     fun OnOkClicked(dialog: DialogFragment)
