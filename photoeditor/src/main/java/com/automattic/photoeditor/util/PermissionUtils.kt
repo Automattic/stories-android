60425e006e19e888638bfc5f1d9b16bd8806ec3e
Oguz Kocer <oguz.kocer@automattic.com>
2021-08-04 23:06:08 -0400

    Remove unused import from PermissionUtils

--- photoeditor/src/main/java/com/automattic/photoeditor/util/PermissionUtils.kt
+++ photoeditor/src/main/java/com/automattic/photoeditor/util/PermissionUtils.kt
@@ -5,7 +5,6 @@ import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Build.VERSION_CODES
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
